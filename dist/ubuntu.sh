a384356a593a12838b57313a7731db6e3fe3fe7b
THEBIGMISHA <esp12p@gmail.com>
2023-01-17 18:10:08 +0300

    23/01/17

---
+++ dist/ubuntu.sh
@@ -0,0 +1,17 @@
+clear
+echo "
+░██████╗░█████╗░██╗░░██╗░█████╗░░█████╗░██╗░░░░░██████╗░░█████╗░████████╗
+██╔════╝██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗╚══██╔══╝
+╚█████╗░██║░░╚═╝███████║██║░░██║██║░░██║██║░░░░░██████╦╝██║░░██║░░░██║░░░
+░╚═══██╗██║░░██╗██╔══██║██║░░██║██║░░██║██║░░░░░██╔══██╗██║░░██║░░░██║░░░
+██████╔╝╚█████╔╝██║░░██║╚█████╔╝╚█████╔╝███████╗██████╦╝╚█████╔╝░░░██║░░░
+╚═════╝░░╚════╝░╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═════╝░░╚════╝░░░░╚═╝░░░
+By: @THEBIGMISHA License: MIT License GitHub: THEBIGMISHA/SchoolBot"
+echo "To start installation enter 'sudo' password, to cancel press 'Ctrl' + 'c'"
+sudo apt install python3
+sudo apt install git
+cd /
+sudo git clone "https://github.com/THEBIGMISHA/SchoolBot" "~/GitHub/l/"
+clear
+echo "Installation completed, the window will close automatically"
+sleep 3
