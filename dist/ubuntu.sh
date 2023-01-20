7d5fa24739b37594e083d095b3ac966055503e55
THEBIGMISHA <esp12p@gmail.com>
2023-01-20 23:46:25 +0300

    23/01/20

--- dist/ubuntu.sh
+++ dist/ubuntu.sh
@@ -1,4 +1,4 @@
-clear
+clear																				#CLEAR CONSOLE		
 echo "
 ░██████╗░█████╗░██╗░░██╗░█████╗░░█████╗░██╗░░░░░██████╗░░█████╗░████████╗
 ██╔════╝██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗╚══██╔══╝
@@ -6,12 +6,16 @@ echo "
 ░╚═══██╗██║░░██╗██╔══██║██║░░██║██║░░██║██║░░░░░██╔══██╗██║░░██║░░░██║░░░
 ██████╔╝╚█████╔╝██║░░██║╚█████╔╝╚█████╔╝███████╗██████╦╝╚█████╔╝░░░██║░░░
 ╚═════╝░░╚════╝░╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═════╝░░╚════╝░░░░╚═╝░░░
-By: @THEBIGMISHA License: MIT License GitHub: THEBIGMISHA/SchoolBot"
-echo "To start installation enter 'sudo' password, to cancel press 'Ctrl' + 'c'"
-sudo apt install python3
-sudo apt install git
-cd /
-sudo git clone "https://github.com/THEBIGMISHA/SchoolBot" "~/GitHub/l/"
-clear
-echo "Installation completed, the window will close automatically"
-sleep 3
+By: @THEBIGMISHA License: MIT License GitHub: THEBIGMISHA/SchoolBot"				#WELCOME AND LOGO	
+echo "To start installation enter 'sudo' password, to cancel press 'Ctrl' + 'c'"	#APPEAL				
+sudo apt install python3															#INSTALL PYTHON3 	(APT)
+sudo apt install python3-pip														#INSTALL PIP MOD 	(APT)
+pip install sqlite3																	#INSTALL SQLITE3 	(PIP)
+pip install PyTelegramBotAPI														#INSTALL TELEBOT 	(PIP)
+sudo snap install sqlitebrowser														#INSTALL SQLiteB.	(SNAP)
+sudo apt install git																#INSTALL GIT	 	(APT)
+cd ~																				#TRANSITION TO PWD	
+git clone "https://github.com/THEBIGMISHA/SchoolBot/"								#GIT CLONE REPO		(GIT)
+clear																				#CLEAR CONSOLE
+echo "Installation completed, the window will close automatically"					#APPEAL
+sleep 3																				#WAIT
