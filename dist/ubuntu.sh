477f22ce6718fa742cb7bbef4cc980abd6a0e666
THEBIGMISHA <esp12p@gmail.com>
2023-01-21 00:06:44 +0300

    23/01/21

--- dist/ubuntu.sh
+++ dist/ubuntu.sh
@@ -1,4 +1,4 @@
-clear																				#CLEAR CONSOLE		
+clear											#CLEAR CONSOLE		
 echo "
 ░██████╗░█████╗░██╗░░██╗░█████╗░░█████╗░██╗░░░░░██████╗░░█████╗░████████╗
 ██╔════╝██╔══██╗██║░░██║██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗╚══██╔══╝
@@ -6,16 +6,16 @@ echo "
 ░╚═══██╗██║░░██╗██╔══██║██║░░██║██║░░██║██║░░░░░██╔══██╗██║░░██║░░░██║░░░
 ██████╔╝╚█████╔╝██║░░██║╚█████╔╝╚█████╔╝███████╗██████╦╝╚█████╔╝░░░██║░░░
 ╚═════╝░░╚════╝░╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═════╝░░╚════╝░░░░╚═╝░░░
-By: @THEBIGMISHA License: MIT License GitHub: THEBIGMISHA/SchoolBot"				#WELCOME AND LOGO	
+By: @THEBIGMISHA License: MIT License GitHub: THEBIGMISHA/SchoolBot"			#WELCOME AND LOGO	
 echo "To start installation enter 'sudo' password, to cancel press 'Ctrl' + 'c'"	#APPEAL				
-sudo apt install python3															#INSTALL PYTHON3 	(APT)
-sudo apt install python3-pip														#INSTALL PIP MOD 	(APT)
-pip install sqlite3																	#INSTALL SQLITE3 	(PIP)
-pip install PyTelegramBotAPI														#INSTALL TELEBOT 	(PIP)
-sudo snap install sqlitebrowser														#INSTALL SQLiteB.	(SNAP)
-sudo apt install git																#INSTALL GIT	 	(APT)
-cd ~																				#TRANSITION TO PWD	
-git clone "https://github.com/THEBIGMISHA/SchoolBot/"								#GIT CLONE REPO		(GIT)
-clear																				#CLEAR CONSOLE
-echo "Installation completed, the window will close automatically"					#APPEAL
-sleep 3																				#WAIT
+sudo apt install python3								#INSTALL PYTHON3 	(APT)
+sudo apt install python3-pip								#INSTALL PIP MOD 	(APT)
+pip install sqlite3									#INSTALL SQLITE3 	(PIP)
+pip install PyTelegramBotAPI								#INSTALL TELEBOT 	(PIP)
+sudo snap install sqlitebrowser								#INSTALL SQLiteB.	(SNAP)
+sudo apt install git									#INSTALL GIT	 	(APT)
+cd ~											#TRANSITION TO PWD	
+git clone "https://github.com/THEBIGMISHA/SchoolBot/"					#GIT CLONE REPO		(GIT)
+clear											#CLEAR CONSOLE
+echo "Installation completed, the window will close automatically"			#APPEAL
+sleep 3											#WAIT
